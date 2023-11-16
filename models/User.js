a50e8d06a3876278621ae5d4dc020678c929ac55
stevendreed <steven.daniel.reed@gmail.com>
2023-11-16 10:43:43 -0800

    changed user.id to a UUID

--- models/User.js
+++ models/User.js
@@ -11,10 +11,11 @@ class User extends Model {
 User.init(
   {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      default: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      // autoIncrement: true,
     },
     name: {
       type: DataTypes.STRING,
