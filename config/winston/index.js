d918279488cf4aec4134c2a97b6ac8df7c44b0bd
stevendreed <steven.daniel.reed@gmail.com>
2023-11-19 10:58:32 -0800

    created simple winston index file

--- config/winston/index.js
+++ config/winston/index.js
@@ -2,3 +2,26 @@
 "use strict";
 
 const winston = require('winston');
+
+const winOptions = 
+    {
+        level: 'info',
+        format: winston.format.json(),
+        defaultMeta: { service: 'user-service' },
+        transports: [
+            new winston.transports.File( 
+                { filename: 'error.log', level: 0}),
+            new winston.transports.File( 
+                { filename: 'combined.log' }),
+            ]
+    }
+
+const logger = winston.createLogger(winOptions);
+
+// ------ debugging section to print verbose logs in console ------- //
+
+logger.add(new winston.transports.Console(
+    {
+        format: winston.format.simple()
+    }
+));
