b9f5086257e3a5f63e1915b74af7827605d64a3e
Mysmelf <matthewr9937@gmail.com>
2023-11-21 15:10:15 -0800

    Increased description datatype length

--- config/connection.js
+++ config/connection.js
@@ -13,17 +13,18 @@ let sequelize;
     }
   }
 );
+
 // } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
+  // sequelize = new Sequelize(
+  //   process.env.DB_NAME,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASSWORD,
+  //   {
+  //     host: 'localhost',
+  //     dialect: 'mysql',
+  //     port: 3306
+  //   }
+  // );
 // }
 
 sequelize
